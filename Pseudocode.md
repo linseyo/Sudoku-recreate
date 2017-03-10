3831b54f95e36cca45e4644d0f737d55ec7d51ee
Joshua Mun & Seth Gerou <pair+joshmun.sethgerou@devbootcamp.com>
2017-03-10 14:53:20 -0800

    Add structure of methods that check arrays of given cell

---
+++ Pseudocode.md
@@ -0,0 +1,15 @@
+Code to check whether a value is possible in an array
+
+is number in array?
+
+def check_array(array)
+array.each {|value| (1..9).each do
+value == number}
+
+
+
+?? How do you check if this is the **only** value possible in this array
+
+
+mega_array.each do
+end
--- sudoku.rb
+++ sudoku.rb
@@ -20,3 +20,50 @@ end
 # form `solve` returns.
 def pretty_board(board)
 end
+
+def method ()
+  mega_array.each_with_index do |row, rposition|
+    row.each_with_index do |cell, cposition|
+      if dash_finder(cell)
+        check_arrays(cell, rposition, cposition)
+        # then do the check on the row
+        # do the column check
+        # do the box check
+      end
+    end
+  end
+end
+
+def check_arrays(cell, rposition, cposition)
+array.each {|value| (1..9).each do
+value == number}
+end
+
+def dash_finder(cell)
+  cell.include?("-")
+end
+
+def horizontal(rposition)
+  mega_array[rposition]
+end
+
+def vertical(cposition)
+  mega_array.transpose[cposition]
+end
+
+def box_check(cell)
+
+end
+
+mega_array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7,8, 9]
+]
+mega_array[0][0] == [row1]
+
+# pseudocode
+# check horizontally, put the possible in the satchel
+# check vertically, if something is not allowed from satchel, take it out
+# check the box, if something not allowed from satchel, take it out
+# if only 1 number is left, replace - with number
