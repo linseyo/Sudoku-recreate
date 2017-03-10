5543dbd1b3062ac83d7412d1b339ea6feb2043b6
Nick Jarin <email": null>
2017-03-10 12:28:03 -0800

    add methods for creating solvable sudoku board

--- sudoku.rb
+++ sudoku.rb
@@ -1,3 +1,45 @@
+def format_board(board_string)
+  convert_dash(format_columns(format_rows(board_string)))
+end
+
+def format_rows(board_string)
+  board_string.scan(/........./)
+end
+
+def format_columns(rows_array)
+  rows_array.map do |row|
+    row.split('')
+  end
+end
+
+def convert_dash(board)
+  board.each do |row|
+    row.map do |cell|
+      if cell == '-'
+        possibilities = cell.gsub!(/-/, '123456789')
+      end
+    end
+  end
+end
+
+# def row_conflict(format_board)
+#   known_row_digits = []
+#   format_board.each do |row|
+#     row.each do |cell|
+#       if cell.length == 1
+#         known_row_digits << cell
+#       end
+#     end
+#   end
+# end
+
+def solved_cells(row)
+  row.each |cell|
+    if cell.length == 1
+
+    end
+end
+
 # Takes a board as a string in the format
 # you see in the puzzle file. Returns
 # something representing a board after
