6f3d07b123f4bfbe2dff963f17b825d238a1ada8
ElreyB <email": null>
2017-03-10 15:03:13 -0800

    add split string into array of arrays

--- sudoku.rb
+++ sudoku.rb
@@ -6,13 +6,35 @@
 def solve(board_string)
   box = []
   if board_string.length != 81
-    "📝 Need more information!!!"
+    return "📝 Need more information!!!"
   else
-    for i in 0..board_string.length-1
+    string_split_holder = board_string.split(//)
+    counter = 0
+    array_to_add = []
+    string_split_holder.each_with_index do|cell,box_index|
+      if counter == 9
+        box.push(array_to_add)
+        counter = 0
+        array_to_add = []
+        next_element = string_split_holder[box_index]
+          if next_element == nil
+            array_to_add.push(cell)
+            box.push(array_to_add)
+            break
+          else
+            array_to_add.push(cell)
+            counter +=1
+          end
+      else
+        array_to_add.push(cell)
+        counter += 1
+      end
+      if box_index == 80
+          box.push(array_to_add)
+      end
     end
     box
   end
-
 end
 
 # Returns a boolean indicating whether
@@ -29,3 +51,8 @@ end
 # form `solve` returns.
 def pretty_board(board)
 end
+
+test = "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"
+test_array = solve(test)
+p test_array
+
