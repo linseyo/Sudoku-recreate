3b40844c5cf5481af0e93623bba387ed919f5e8f
Dillon Estelle <email": null>
2017-03-10 11:58:26 -0800

    add pretty board

--- sudoku.rb
+++ sudoku.rb
@@ -19,4 +19,32 @@ end
 # The input board will be in whatever
 # form `solve` returns.
 def pretty_board(board)
+  pretty_board = ""
+  counter = 0
+  box_counter = 0
+  row_counter = 0
+    board.split("").each_with_index do |cell,position|
+      if counter == 8
+        pretty_board += " #{cell} \n"
+        counter = 0
+        box_counter = 0
+        row_counter += 1
+        next_element = board[position+1]
+        if row_counter == 3 && next_element != nil
+          pretty_board += "="* 36 + "\n"
+          row_counter = 0
+        end
+      elsif box_counter == 2
+        pretty_board += " #{cell} ||"
+        box_counter = 0
+        counter += 1
+      else
+        pretty_board += " #{cell} |"
+        counter += 1
+        box_counter +=1
+      end
+
+    end
+    pretty_board
 end
+
