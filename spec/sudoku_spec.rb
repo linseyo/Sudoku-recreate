d0978e1e89beb7787c6330eb8c0c82b6578ca41e
Grace Noh & Linsey R <pair+gracenoh153.linseyo@devbootcamp.com>
2017-03-10 18:39:02 -0800

    fix rspec later

--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -3,6 +3,7 @@ require_relative '../sudoku'
 describe "Sudoku" do
   let (:board) {"1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--"}
   let (:board2) {"897465321897465321897465321897465321897465321897465321897465321897465321897465321"}
+  let (:board_string) {"----------2-65-------18--4--9----6-4-3---57-------------------73------9----------"}
 
   context 'sudoku game has ended' do
     it 'returns false when board contains non-integer numbers' do
@@ -12,9 +13,20 @@ describe "Sudoku" do
       expect(solved?(board2)).to eq true
     end
   end
-  context 'builds nested arrays'
-  it 'returns nested arrays' do
-    expect
-  end
+
+  # context 'builds nested arrays'
+  # it 'returns nested arrays' do
+  #   expect(solve(board_string)).to be [
+  #     [-,-,-,-,-,-,-,-,-],
+  #     [-,2,-,6,5,-,-,-,-],
+  #     [-,-,-,1,8,-,-,4,-],
+  #     [-,9,-,-,-,-,6,-,4],
+  #     [-,3,-,-,-,5,7,-,-],
+  #     [-,-,-,-,-,-,-,-,-],
+  #     [-,-,-,-,-,-,-,-,7],
+  #     [3,-,-,-,-,-,-,9,-],
+  #     [-,-,-,-,-,-,-,-,-]
+  #   ]
+  # end
 
 end
