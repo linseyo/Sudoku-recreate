e6ae0651b1062a7a015cdc39bd717cfdde9d31fe
Jordan Levine <jordan@strofina.com>
2017-03-12 13:12:44 -0700

    Add rspec tests for populate board method

--- spec/sudoku_spec.rb
+++ spec/sudoku_spec.rb
@@ -68,19 +68,39 @@ describe "Sudoku" do
   end   
   
 # TESTS POPULATE BOARD
- let(:game_string) { "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--" }
-  context 'when creates and populates the gameboard' do
+  
+  context 'when creates and populates the game board' do
+  
+    let(:game_string) { "1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--" }
+
     it "when dash, puts string with all possibilities in cell - 1st row" do
       expect(populate_board(game_string)[0][4]).to eq('123456789')
     end
+
     it "when # given, puts string with that # in cell - 1st row" do
       expect(populate_board(game_string)[0][2]).to eq('5')
     end
+
     it "when dash, puts string with all possibilities in cell - last row" do
       expect(populate_board(game_string)[8][2]).to eq('123456789')
     end
+
     it "when # given, puts string with that # in cell - last row" do
       expect(populate_board(game_string)[8][3]).to eq('3')
+    end
+
+    it "puts correct value in cell" do
+      string_index = 0
+      for row_index in 0..8
+        for col_index in 0..8
+          if game_string[string_index] == '-'
+            expect(populate_board(game_string)[row_index][col_index]).to eq('123456789')
+          else
+            expect(populate_board(game_string)[row_index][col_index]).to eq(game_string[string_index])
+          end
+          string_index += 1
+        end
+      end
     end
   end
 end
